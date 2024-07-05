57aac6a75073eca5f01854f75dfae666cbc45e91
L Carver <wifihum@gmail.com>
2024-07-04 20:31:01 -0700

    handle GET items list and GET item by id

---
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    // handles route https://localhost:5001/items
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            var item = items.Where(item => item.Id == id).SingleOrDefault();
+            return item;
+        }
+    }
+}
